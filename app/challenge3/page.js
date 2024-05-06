7d802737a477eb35594c20097f218ceaa29e4b63
kiat <kiat@buymall.com.my>
2024-05-06 18:20:49 +0800

    no message

---
+++ app/challenge3/page.js
@@ -0,0 +1,37 @@
+'use client';
+
+import {useEffect} from "react";
+
+export default function Challenge3() {
+
+  const twoSum = (numbers,target)=>
+  {
+    for (let i = 0; i < numbers.length; i++)
+    {
+        for (let j = 0; j < numbers.length; j++)
+        {
+            if(i==j)
+            {
+              continue;
+            }
+
+            if(numbers[i]+numbers[j]==target)
+            {
+                return [(i+1),(j+1)];
+            }
+        }
+    }
+    return [];
+  }
+
+  useEffect(function () {
+      console.log(twoSum([1,5,2,10],7));
+  },[]);
+
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      Challenge 3
+    </main>
+  );
+}
